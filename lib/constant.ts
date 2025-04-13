0e17b7d24d71c075e417efa071843b506d0e438f
remonhasanapu <remon.hasan@technovista.com.bd>
2025-04-13 15:41:47 +0600

    added initial layouts

---
+++ lib/constant.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.NEXT_APP_PUBLIC_NAME || 'Amazona'
+export const APP_SLOGAN = process.env.NEXT_APP_PUBLIC_SLOGAN || 'Spend less, Enjoy more'
+export const APP_DESCRIPTION = process.env.NEXT_APP_PUBLIC_DESCRIPTION || 'Build with Next.js and MongoDB'
